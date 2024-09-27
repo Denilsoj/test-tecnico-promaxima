c157463d158aef3d61236fa021024161ea033739
Denilson Oliveira <112564999+Denilsoj@users.noreply.github.com>
2024-09-27 18:57:38 -0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-// Importando o Vuetify e os estilos
+
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
-// Importando ícones e temas (opcional)
+
 import '@mdi/font/css/materialdesignicons.css';
 
-// Criação da instância do Vuetify
+
 const vuetify = createVuetify({
     components,
     directives,
@@ -18,7 +18,7 @@ const vuetify = createVuetify({
   
 const app = createApp(App);
 
-// Registrando o Vuetify na aplicação
+
 app.use(vuetify);
 
 app.mount('#app');
